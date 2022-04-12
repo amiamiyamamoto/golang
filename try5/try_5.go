e4524a95f8275dc6978bb52319fd052fcefb828e
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-04-12 23:47:16 +0900

    flagパッケージ

---
+++ try5/try_5.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var msg = flag.String("msg", "デフォルト値", "説明")
+var n int
+
+func init() {
+	//ポインタを指定して設定を予約
+	flag.IntVar(&n, "n", 1, "回数")
+}
+
+func main() {
+	flag.Parse()
+	fmt.Println(strings.Repeat(*msg, n))
+}
