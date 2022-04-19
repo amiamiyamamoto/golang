f459fd2bf4b1fedd00524186f68e18b60dccc913
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-04-19 17:23:20 +0900

    Scanner追加

--- try5/try_5.go
+++ try5/try_5.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
-	"strings"
+	"os"
 )
 
-var msg = flag.String("msg", "デフォルト値", "説明")
-var n int
+// var msg = flag.String("msg", "デフォルト値", "説明")
+var n = flag.Bool("n", false, "line number option")
 
-func init() {
-	//ポインタを指定して設定を予約
-	flag.IntVar(&n, "n", 1, "回数")
-}
+// func init() {
+// 	//ポインタを指定して設定を予約
+// 	flag.IntVar(&n, "n", 1, "回数")
+// }
 
 func main() {
 	flag.Parse()
-	fmt.Println(strings.Repeat(*msg, n))
+	// fmt.Println(strings.Repeat(*msg, n))
+
+	//引数の数だけループする
+	for _, arg := range flag.Args() {
+		// fmt.Println(arg)
+		err := readFile(arg)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	fmt.Println(*n)
+}
+
+func readFile(fn string) error {
+	f, err := os.Open(fn)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		fmt.Println(s.Text())
+	}
+	return nil
 }
