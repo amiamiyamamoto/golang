abfb4d3e42a74b4df10d65215bc12191a04486a3
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-04-20 22:12:10 +0900

    ディレクトリ名を渡すとファイル名を出力する

--- try5/try_5_2/try_5_2.go
+++ try5/try_5_2/try_5_2.go
@@ -3,11 +3,30 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 )
 
 func main() {
 	flag.Parse()
 	args := flag.Args()
 	fmt.Println(args)
+
+	files, err := ioutil.ReadDir(flag.Args()[0])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, file := range files {
+		// if file.IsDir() {
+		// 	fmt.Println(file)
+		// }
+		fmt.Println(file.Name())
+
+	}
+	// 指定したディレクトリ以下のJPGファイルをPNGに変換するコマンドを作成する
+	// 自作パッケージ
+	// ユーザ定義型を作成
+	// GoDocを生成
+	// Go Modulesを使う
 
 }
