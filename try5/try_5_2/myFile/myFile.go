8ef0e1288dfefad1014b660ca54cf52627e03bb8
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-04-23 15:50:28 +0900

    拡張子を返すメソッドを追加

--- try5/try_5_2/myFile/myFile.go
+++ try5/try_5_2/myFile/myFile.go
@@ -3,16 +3,28 @@ package myFile
 import (
 	"fmt"
 	"io/ioutil"
+	"path/filepath"
 )
 
 type File string
 
 // .jpgもしくは.jpegファイルだった場合はtrueを返す
 func (f File) IsJpg() bool {
-	if f == "./try_5_2.go" {
+	e := f.GetExt()
+	if e == ".jpg" || f == ".jpeg" {
 		return true
 	}
 	return false
+}
+
+// func (f *File) changeJpgToPng()
+
+// 拡張子を返す関数をつくる
+// IsJpgでこの関数を使う
+// changeJpgToPngメソッドをつくる→実際のファイル名操作、自身のファイル名を変更する
+func (f File) GetExt() string {
+	e := filepath.Ext(string(f))
+	return e
 }
 
 func PrintFile(dir string) {
