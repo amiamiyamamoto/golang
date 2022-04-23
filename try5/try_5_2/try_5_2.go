94f3459079688a804be069c863ca9a5e84b12212
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-04-23 16:55:09 +0900

    不要な変数を削除

--- try5/try_5_2/try_5_2.go
+++ try5/try_5_2/try_5_2.go
@@ -2,20 +2,13 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	myfile "myfile/myFile"
 )
 
 func main() {
 	flag.Parse()
-	args := flag.Args()
-	fmt.Println(args)
+	// args := flag.Args()
 
-	// 指定したディレクトリ以下のJPGファイルをPNGに変換するコマンドを作成する
-	// 自作パッケージ
-	// ユーザ定義型を作成
-	// GoDocを生成
-	// Go Modulesを使う
-	myfile.PrintFile(flag.Args()[0])
+	myfile.ChangeFilesJpgToPng(flag.Args()[0])
 
 }
