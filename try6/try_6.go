ce4a45281d4b01056bb490a1175535d774143f5c
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-04-25 17:37:07 +0900

    インターフェースの作成

--- try6/try_6.go
+++ try6/try_6.go
@@ -1,18 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
 
 type Stringer interface {
 	String() string
 }
 
+// ユーザ定義型のStringerInt型にStringメソッドを実装
 type StringerInt int
 
-func (s StringerInt) Stringer() string {
-	return string(s)
+func (s StringerInt) String() string {
+	return strconv.Itoa(int(s))
+}
+
+// floatでStringer interfaceを実装する
+type StringerFloat64 float64
+
+func (s StringerFloat64) String() string {
+	return strconv.FormatFloat(float64(s), 'f', -1, 64)
 }
 
 func main() {
-	var s StringerInt = 10
-	fmt.Println(s.Stringer())
+	var i StringerInt = -10
+	var f StringerFloat64 = 12.34
+
+	// Stringerのスライスを作成し、ループする
+	var list []Stringer = []Stringer{i, f}
+	for _, i := range list {
+		fmt.Println(getType(i))
+	}
+
+}
+
+// Stringer interface型を受け取り、具象型名と値を返す
+func getType(s Stringer) string {
+	type_and_value := "type: " + reflect.TypeOf(s).Name() + ", value: " + s.String()
+	return type_and_value
 }
