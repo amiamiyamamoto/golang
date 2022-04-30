39855cd81610fbdeca79ebddd1c8a54285bdd6f5
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-04-30 16:07:28 +0900

    errorをユーザ定義型で作成

--- try7/try_7_1_1/try_7_1_1.go
+++ try7/try_7_1_1/try_7_1_1.go
@@ -1,14 +1,38 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Stringer interface {
 	String() string
 }
 
 func main() {
+	// intのiを定義
+	var i int = 10
+	// ToStringerメソッドでiをStringer型に変換
+	si, err := ToStringer(i)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(si.String())
 
 }
 
 // 任意の値をStringer型に変換する関数
 func ToStringer(v interface{}) (Stringer, error) {
+	if s, ok := v.(Stringer); ok {
+		return s, nil
+	}
+	return nil, MyError("CastError")
+}
 
+// error interefaceを実装したユーザ定義型
+type MyError string
+
+func (e MyError) Error() string {
+	return string(e)
 }
