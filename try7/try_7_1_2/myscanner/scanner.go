74f3b149d252b1b03687a4ff508e7a8beb630c0e
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-05-05 23:45:37 +0900

    Scanner拡充

--- try7/try_7_1_2/myscanner/scanner.go
+++ try7/try_7_1_2/myscanner/scanner.go
@@ -1,18 +1,47 @@
 package myscanner
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 type Scanner struct {
-	text   string
-	reader io.Reader
+	rune    rune
+	reader  io.Reader
+	length  uint // 受け取った文字列長を格納
+	current uint // 現在読み込んでいる文字位置を格納
 }
 
+// スキャンする文字がまだある場合はtrueを返す
+func (s *Scanner) Scan() bool {
+	return true
+}
+
+// 現在の位置の文字を一つ返す
+func (s *Scanner) Text() string {
+	// s.text.
+	return "a"
+}
+
+// io.Readerを受け取り、Scannerのポインタを返す
 func NewScanner(r io.Reader) *Scanner {
-	// inf := ^uint(0)
-	// for i := uint(666); i < inf; i++ {
-	// 	println("tako")
-	// }
 	var s Scanner
 	s.reader = r
+	// スライスを確保
+	//どれくらいの長さになるか事前に確認したいが一旦20で固定
+	// 丁度いい長さのスライスを渡したい
+	p := make([]byte, 20)
+	_, err := r.Read(p)
+	fmt.Printf("%+v\n %+v", p, err)
+
+	for _, byte := range p {
+		// fmt.Println(string(byte))
+		// fmt.Println(byte)
+		print(byte)
+		fmt.Sprintf("%s", p)
+	}
+
+	fmt.Println("\n" + string(p))
+
 	return &s
 }
--- try7/try_7_1_2/try_7_1_2.go
+++ try7/try_7_1_2/try_7_1_2.go
@@ -9,4 +9,5 @@ func main() {
 
 	// fmt.Println(myscanner.NewScanner(strings.NewReader("あいうえお")))
 	myscanner.NewScanner(strings.NewReader("あいうabc123"))
+	// fmt.Printf("%+v\n", *a)
 }
