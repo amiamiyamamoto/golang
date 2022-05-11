12c9da2ac389f1b23280d78ebaee7e0c455322fb
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-05-11 23:43:09 +0900

    Scannerの実処理部分追加

--- try7/try_7_1_2/myscanner/scanner.go
+++ try7/try_7_1_2/myscanner/scanner.go
@@ -3,31 +3,41 @@ package myscanner
 import (
 	"fmt"
 	"io"
+	"unicode/utf8"
 )
 
 type Scanner struct {
 	// rune    rune
-	Data    []byte
-	reader  io.Reader //いらないかも
-	length  uint      // 受け取った文字列長を格納
-	current uint      // 現在読み込んでいる文字位置を格納
+	text    string
+	length  uint // 受け取った文字列長を格納
+	current uint // 現在読み込んでいる文字位置を格納
+	error   error
 }
 
 // スキャンする文字がまだある場合はtrueを返す
 func (s *Scanner) Scan() bool {
+	//文字を最後まで読み込んでいた場合はfalseを返す
+	if s.current == s.length {
+		return false
+	}
+	// エラーを保持している場合はfalseを返す
+	if s.error != nil {
+		return false
+	}
 	return true
 }
 
 // 現在の位置の文字を一つ返す
 func (s *Scanner) Text() string {
-	// s.text.
-	return "a"
+	//s.current文字目（先頭が0）の文字を取り出す
+	char := string(getRuneAt(s.text, s.current))
+	s.current++
+	return char
 }
 
 // io.Readerを受け取り、Scannerのポインタを返す
-func NewScanner(r io.Reader) (*Scanner, error) {
+func NewScanner(r io.Reader) *Scanner {
 	var s Scanner
-	s.reader = r
 	// スライスを確保
 	var pl int = 100
 	println(pl)
@@ -40,7 +50,9 @@ func NewScanner(r io.Reader) (*Scanner, error) {
 		len, err = r.Read(tmp)
 
 		if err != nil {
-			return nil, err
+			// エラーが起こった場合はstructのerrorに値を保持し処理を終了させる。
+			s.error = err
+			return nil
 		}
 
 		// 取得した値をpに追加する
@@ -50,18 +62,17 @@ func NewScanner(r io.Reader) (*Scanner, error) {
 		// fmt.Println(tmp)
 
 	}
-	s.Data = p
-	//TODO:取得した[]byteを　string → rune に変換してScannerに代入する
-	// fmt.Println(len)
+	// strに直してscannerに代入
+	txt := string(p)
+	s.text = txt
+	// テキストの文字数を代入
+	s.length = uint(utf8.RuneCountInString(txt))
+	// fmt.Println("\n" + string(p))
 
-	// for _, byte := range p {
-	// 	// fmt.Println(string(byte))
-	// 	// fmt.Println(byte)
-	// 	print(byte)
-	// 	fmt.Sprintf("%s", p)
-	// }
+	return &s
+}
 
-	fmt.Println("\n" + string(p))
-
-	return &s, nil
+func getRuneAt(s string, i uint) rune {
+	rs := []rune(s)
+	return rs[i]
 }
