eb271c30214f672ac9ca37fb4c5eec31858e9eb5
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-05-11 23:43:29 +0900

    Scannerをメインで呼び出し

--- try7/try_7_1_2/try_7_1_2.go
+++ try7/try_7_1_2/try_7_1_2.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"scanner/myscanner"
 	"strings"
 )
@@ -9,7 +10,27 @@ import (
 func main() {
 
 	// fmt.Println(myscanner.NewScanner(strings.NewReader("あいうえお")))
-	s, _ := myscanner.NewScanner(strings.NewReader("あいうabc123"))
+	// s, _ := myscanner.NewScanner(strings.NewReader("あいうabc123"))
 	// fmt.Printf("%+v\n", *a)
-	fmt.Println(string(s.Data))
+	// fmt.Println(string(s.Data))
+	readMyScanner()
+}
+
+func readMyScanner() {
+	scanner := myscanner.NewScanner(strings.NewReader("あいうabc123"))
+	// if err != nil {
+	// 	fmt.Fprintln(os.Stderr, err)
+	// }
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	// NewScannerではエラーを返さない
+	// readメソッドでエラーがあった場合は、Scanner structにエラー値をもたせるconst
+	// エラーを保持している場合は、Scanメソッドはfalseを返す
+	// Errメソッドを作成して、どうやって返せばいいんだ？
 }
