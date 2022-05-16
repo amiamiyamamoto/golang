70868507c2c5a1732ef211d24892aaf069839f74
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-05-16 23:31:14 +0900

    標準入力から受け取って出力する

--- try9/try_9.go
+++ try9/try_9.go
@@ -16,12 +16,18 @@ func main() {
 }
 
 func input(r io.Reader) <-chan string {
+	//TODO: チャネルをつくる
+	var ch chan string
 	go func() {
 		s := bufio.NewScanner(r)
 		for s.Scan() {
 			//TODO: チャネルに読み込んだ文字列を送る
+			ch <- s.Text()
 		}
 		//TODO: チャネルを閉じる
+		close(ch)
 	}()
 	//TODO: チャネルを返す
+	return ch
+
 }
