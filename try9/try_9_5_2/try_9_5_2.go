89a3fadaaafface08afe24bab2cdeda006adad83
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-05-29 14:39:41 +0900

    forの中身を関数化

--- try9/try_9_5_2/try_9_5_2.go
+++ try9/try_9_5_2/try_9_5_2.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -17,26 +18,22 @@ func main() {
 }
 
 func download(url string, fn string) error {
-	client := &http.Client{ // CheckRedirect: redirectPolicyFunc,
-	}
 
 	// rangeで分割して複数回ダウンロードする
-	r := 500 // 分割するサイズ
-	c := 0   // 現在位置
-	// 0-500, 501-1000, 1001-1500
-	for _, num := range []int{1, 2, 3} {
-		req, err := http.NewRequest("GET", url, nil)
-		req.Header.Add("Range", "bytes="+string(c)+"-"+string(c+r))
-		c = r + 1
-	}
+	var r uint = 100000 // 分割するサイズ
+	var c uint = 0      // 現在位置
+	// var resp *http.Response
 
-	// TODO:Doもfor内で実行する
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
+	// status code 406でforを終了させる
+	for _, _ = range []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10} {
+		resp, err := rangeReq(url, r, c)
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode == 406 {
+			break
+		}
 	}
-	// TODO:for内でdeferしないよう関数化する
-	defer resp.Body.Close()
 
 	out, err := os.Create(fn)
 	if err != nil {
@@ -48,6 +45,23 @@ func download(url string, fn string) error {
 	return err
 }
 
-func rangeReq() {
+func rangeReq(url string, r uint, c uint) (*http.Response, error) {
+	client := &http.Client{ // CheckRedirect: redirectPolicyFunc,
+	}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Range", "bytes="+strconv.Itoa(c)+"-"+strconv.Itoa(c+r))
+	c = c + r + 1
 
+	// TODO:Doもfor内で実行する
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(resp.Status)
+	// TODO:for内でdeferしないよう関数化する
+	defer resp.Body.Close()
+	return resp, nil
 }
